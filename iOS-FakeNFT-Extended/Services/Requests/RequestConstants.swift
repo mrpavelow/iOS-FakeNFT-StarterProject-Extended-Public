cb12cc7c4faf67b2c64d8da7756a4507f59bcb7f
SannikovVladislav <sannikovladislav@mail.ru>
2026-04-08 23:33:52 +0300

    Реализация ячейки пользователя и добавлена модель User

--- iOS-FakeNFT-Extended/Services/Requests/RequestConstants.swift
+++ iOS-FakeNFT-Extended/Services/Requests/RequestConstants.swift
@@ -1,5 +1,4 @@
 enum RequestConstants {
     static let baseURL = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net"
-#warning("insert your token here")
-    static let token = ""
+    static let token = "315ba15d-369e-472a-98db-74a76ef4676f"
 }
